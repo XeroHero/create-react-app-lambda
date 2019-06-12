e67648532f028b1c84f7b498133da431b33d2cf0
Lorenzo Battilocchi <lorenzo.battilocchi@ucdconnect.ie>
2019-06-12 19:41:22 +0200

    Test JS Script

--- src/App.js
+++ src/App.js
@@ -1,50 +1,74 @@
 import React, { Component } from "react"
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 import logo from "./logo.svg"
 import "./App.css"
+import FacebookLogin from './facebook.js';
+import FacebookLoginWithButton from './facebook-with-button.js';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+const e = React.createElement;
+const responseFacebook = (response) => {
+  console.log(response);
+};
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
+class Base extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+      <div>
+        <Link to="/dummy">Route to dummy page</Link>
+
+        <div>
+          <p>Facebook login with default button and styling</p>
+          <FacebookLoginWithButton
+            appId="490652501351020"
+            autoLoad
+            callback={responseFacebook}
+            icon="fa-facebook"
+          />
+        </div>
+
+        <div>
+          <p>Facebook login with render prop (and no styling provided out the box)</p>
+          <FacebookLogin
+            appId="490652501351020"
+            autoLoad
+            callback={responseFacebook}
+            render={renderProps => (
+              <button onClick={renderProps.onClick}>This is my custom FB button</button>
+            )}
+          />
+        </div>
       </div>
-    )
+    );
   }
 }
+
+class Dummy extends Component {
+  render() {
+    return (
+      <div>
+        <Link to="/">Back</Link>
+        <h1>
+          This is just a dummy page to test the button<br />
+          <a href="https://fb.com/">
+            survives back and forth routing
+          </a>
+        </h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={Base}/>
+    <Route path="/dummy" component={Dummy}/>
+  </Router>,
+  document.getElementById('demo')
+);
+
+const domContainer = document.querySelector('#fbContainer');
+ReactDOM.render(e(fbLoginIndex), domContainer);
 
 export default App
