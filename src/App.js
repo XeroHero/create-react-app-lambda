63a0716c49973cf59b761d44dece9c530062424b
Lorenzo Battilocchi <lorenzo.battilocchi@ucdconnect.ie>
2019-06-12 20:06:42 +0200

    Call JS script

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { Component } from "react"``
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import { Link } from 'react-router-dom';
