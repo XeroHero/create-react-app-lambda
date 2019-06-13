19ce8957dc471c9c6526b100dce6002680b9f2bd
Lorenzo Battilocchi <lorenzo.battilocchi@ucdconnect.ie>
2019-06-14 00:35:44 +0200

    Implementing FB Login Button

--- public/src/App.js
+++ public/src/App.js
@@ -1,74 +1,30 @@
-import React, { Component } from "react";
-import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-import { Link } from 'react-router-dom';
-import logo from "./logo.svg"
-import "./App.css"
-import FacebookLogin from './facebook.js';
-import FacebookLoginWithButton from './facebook-with-button.js';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const e = React.createElement;
-const responseFacebook = (response) => {
-  console.log(response);
-};
+import Facebook from './components/facebook';
 
-class Base extends Component {
-  render() {
-    return (
-      <div>
-        <Link to="/dummy">Route to dummy page</Link>
-
-        <div>
-          <p>Facebook login with default button and styling</p>
-          <FacebookLoginWithButton
-            appId="490652501351020"
-            autoLoad
-            callback={responseFacebook}
-            icon="fa-facebook"
-          />
-        </div>
-
-        <div>
-          <p>Facebook login with render prop (and no styling provided out the box)</p>
-          <FacebookLogin
-            appId="490652501351020"
-            autoLoad
-            callback={responseFacebook}
-            render={renderProps => (
-              <button onClick={renderProps.onClick}>This is my custom FB button</button>
-            )}
-          />
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Facebook Auth Example</h1>
+        <p>
+          To get started, authenticate with Facebook.
+        </p>
+        <Facebook />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-class Dummy extends Component {
-  render() {
-    return (
-      <div>
-        <Link to="/">Back</Link>
-        <h1>
-          This is just a dummy page to test the button<br />
-          <a href="https://fb.com/">
-            survives back and forth routing
-          </a>
-        </h1>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={Base}/>
-    <Route path="/dummy" component={Dummy}/>
-  </Router>,
-  document.getElementById('demo')
-);
-
-const domContainer = document.querySelector('#fbContainer');
-ReactDOM.render(e(fbLoginIndex), domContainer);
-
-export default App
+export default App;
